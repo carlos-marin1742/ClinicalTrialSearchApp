13dca652ad1ec5d726b999c1a80e7a9fd6fcf7f1
Carlos Marin <carlos.a_marin@yahoo.com>
2026-04-23 09:31:57 -0500

    client.js cleanup

--- server/db/client.js
+++ server/db/client.js
@@ -2,15 +2,12 @@ import pg from 'pg';
 import { config } from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 config({ path: path.join(__dirname, '../../.env') });
 
 
 // pool manages multiple clients and connections to the database, allowing for efficient query
 // This is important when managing multiple users are trying to bookmark trials
-console.log('Connecting to:', process.env.DATABASE_URL);
-
 import { Pool } from 'pg';
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
